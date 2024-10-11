14a562d60d3b872d196b3a4a498ef2904ab2997a
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-10-11 12:32:59 +0300

    first commit

---
+++ webapp/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms.widgets import PasswordInput,TextInput
+from .models import Record
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','password1','password2']
+
+class LoginForm(AuthenticationForm):
+
+    username=forms.CharField(widget=TextInput())
+    password=forms.CharField(widget=PasswordInput())
+
+class CreateRecordForm(forms.ModelForm):
+    class Meta:
+        model=Record
+        fields=['first_name','last_name','phone','category','tall','width','address']
+
+class UpdateRecordForm(forms.ModelForm):
+    class Meta:
+        model=Record
+        fields=['first_name','last_name','phone','category','tall','width','address']
